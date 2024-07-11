108d9ce1efed39f9118c64986e261813d0b4ec0c
heliosa123 <heliosaatmaja@gmail.com>
2024-07-11 14:47:45 +0700

    kedua

--- config.js
+++ config.js
@@ -1,22 +1,39 @@
 const admin = require('firebase-admin');
-require('dotenv').config();
+require('dotenv').config(); // Ensure environment variables are loaded
 
+const {
+  TYPE,
+  PROJECT_ID,
+  PRIVATE_KEY_ID,
+  PRIVATE_KEY,
+  CLIENT_EMAIL,
+  CLIENT_ID,
+  AUTH_URI,
+  TOKEN_URI,
+  AUTH_PROVIDER_X509_CERT_URL,
+  CLIENT_X509_CERT_URL,
+  DATABASE_URL
+} = process.env;
 
-// Initialize Firebase Admin SDK using environment variables
+// Check for missing environment variables and provide default values or throw an error
+if (!TYPE || !PROJECT_ID || !PRIVATE_KEY_ID || !PRIVATE_KEY || !CLIENT_EMAIL || !CLIENT_ID || !AUTH_URI || !TOKEN_URI || !AUTH_PROVIDER_X509_CERT_URL || !CLIENT_X509_CERT_URL || !DATABASE_URL) {
+  throw new Error('Missing environment variables');
+}
+
 admin.initializeApp({
   credential: admin.credential.cert({
-    type: process.env.type,
-    project_id: process.env.project_id,
-    private_key_id: process.env.private_key_id,
-    private_key: process.env.private_key.replace(/\\n/g, '\n'), // Replace escaped newlines
-    client_email: process.env.client_email,
-    client_id: process.env.client_id,
-    auth_uri: process.env.auth_uri,
-    token_uri: process.env.token_uri,
-    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-    client_x509_cert_url: process.env.client_x509_cert_url
+    type: TYPE,
+    project_id: PROJECT_ID,
+    private_key_id: PRIVATE_KEY_ID,
+    private_key: PRIVATE_KEY.replace(/\\n/g, '\n'), // Handle escaped newlines in private key
+    client_email: CLIENT_EMAIL,
+    client_id: CLIENT_ID,
+    auth_uri: AUTH_URI,
+    token_uri: TOKEN_URI,
+    auth_provider_x509_cert_url: AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: CLIENT_X509_CERT_URL
   }),
-  databaseURL: process.env.databaseURL
+  databaseURL: DATABASE_URL
 });
 
 const db = admin.firestore();
